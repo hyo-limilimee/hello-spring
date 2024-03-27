c2edb9bce09f99810f78b009b2f09509c626ef01
hyo-limilimee <1001lily@naver.com>
2024-03-27 19:14:18 +0900

    feat: apply object-oriented principle - new discount policy apply and problem

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,18 +2,21 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceImpl implements  OrderService{
+public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();    // 추상, 구현 모두 의존 -> DIP 위반
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();   // OCP 위반
+    private DiscountPolicy discountPolicy;  // 인터페이스에만 의존
 
     @Override
-    public Order createOrder(Long memberId, String itemName, int itemPrice){
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
