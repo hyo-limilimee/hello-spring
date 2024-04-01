e75a958eac65f767ca4e0ca9cee50c34422f9d96
hyo-limilimee <1001lily@naver.com>
2024-04-01 22:10:58 +0900

    feat: singleton container - singleton pattern

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -25,4 +25,18 @@ public class SingletonTest {
         // memberService1 != memberService2
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isEqualTo(singletonService2);
+        // same == (인스턴스를 비교)
+        // equal 자바의 equals
+    }
 }
