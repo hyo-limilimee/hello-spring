f44e0738441a71aa605ba8c050600abbde862265
hyo-limilimee <1001lily@naver.com>
2024-04-03 20:58:50 +0900

    feat: component scan - option handling

---
+++ core/src/test/java/hello/core/autowirde/AutoWiredTest.java
@@ -0,0 +1,37 @@
+package hello.core.autowirde;
+
+import hello.core.member.Member;
+import jakarta.annotation.Nullable;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoWiredTest {
+
+    @Test
+    void AutoWiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        
+        // 호출 안 됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+        // null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        // Optional.empty 호출
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
