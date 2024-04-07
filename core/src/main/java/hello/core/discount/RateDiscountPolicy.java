1204c7ae0e9673bacd584d630102e6b2ff63a402
hyo-limilimee <1001lily@naver.com>
2024-04-08 00:47:52 +0900

    feat: component scan - make Annotation

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,9 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
-
     private int discountPercent = 10;   // 10% 할인
 
     @Override
@@ -20,6 +20,4 @@ public class RateDiscountPolicy implements DiscountPolicy {
             return 0;
         }
     }
-
-
 }
