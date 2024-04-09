73082610f513a74d2fd1f7e8541c783d5dce1bc4
hyo-limilimee <1001lily@naver.com>
2024-04-09 20:09:40 +0900

    feat: bean scope - request scope example

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,20 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+}
